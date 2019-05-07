03e4c8c554e341f70410bfa1a030eed53f81cb6b
Fernando Aguilar <fernando.aguilar@hotmail.com.br>
2019-05-07 04:25:33 -0300

    Add Redux and store

--- pokesystem/app/javascript/components/App.js
+++ pokesystem/app/javascript/components/App.js
@@ -1,16 +1,24 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import { Provider } from "react-redux"
+
 import Pokemon from './Pokemon';
 
-export class App extends React.Component {
+import configureStore from '../configureStore'
+const store = configureStore();
+
+class App extends React.Component {
   render() {
     return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" render={() => ("Home!")} />
-        <Route path="/pokemon" render={() => <Pokemon name="ditto" url="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png" />}></Route>
-      </Switch>
-    </BrowserRouter>);
+      <Provider store={(store)}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" render={() => ("Home!")} />
+            <Route path="/pokemon" render={() => <Pokemon name="ditto" url="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png" />}></Route>
+          </Switch>
+        </BrowserRouter>
+      </Provider>);
   }
 }
 
