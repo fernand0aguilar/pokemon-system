b48419a815aff124124ef914cf4a61be8abbcb6e
Fernando Aguilar <fernando.aguilar@hotmail.com.br>
2019-05-08 03:28:35 -0300

    Add improvements and new components

---
+++ pokesystem/app/javascript/components/Footer.js
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+const Footer = () => (
+    <footer>
+        <small>&copy; Pokedex Inc.</small>
+    </footer>
+);
+
+export default Footer;
---
+++ pokesystem/app/javascript/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const Header = () => (
+    <header>
+        <h1>Pokedex System !</h1>
+    </header>
+);
+
+export default Header;
