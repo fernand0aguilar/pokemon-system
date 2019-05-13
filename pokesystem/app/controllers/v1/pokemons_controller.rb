a8f4fbc01708d3db908e3e5a19c8aa6cdd49cbad
Fernando Aguilar <fernando.aguilar@hotmail.com.br>
2019-05-13 11:00:13 -0300

    refactoring controller

--- pokesystem/app/controllers/v1/pokemons_controller.rb
+++ pokesystem/app/controllers/v1/pokemons_controller.rb
@@ -5,24 +5,36 @@ class V1::PokemonsController < ApplicationController
     def index
         pokemons = Pokemon.all
         get_pokemons_from_api
+
+        File.open("temp.json","w") do |f|
+            f.write(pokemons.to_json)
+        end
+
         render json: pokemons
     end
 
     def get_evolution_chain(pokemon_species)
-        evolution_chain = pokemon_species.evolution_chain.get.chain
+        begin
+            evolution_chain = pokemon_species.evolution_chain.get.chain
 
-        firstGen = evolution_chain.evolves_to
-        secondGen = evolution_chain.evolves_to.first.evolves_to
-        thirdGen = evolution_chain.evolves_to.first.evolves_to.first.evolves_to
-        
-        if firstGen.first.species.name != pokemon_species.name && secondGen.first.species.name != pokemon_species.name
-            evolution = firstGen.first.species.name
-        elsif firstGen.first.species.name == pokemon_species.name
-            evolution = secondGen.first.species.name
-        end
+            if evolution_chain.evolves_to != nil
+                firstGen = evolution_chain.evolves_to
+            end
+            if evolution_chain.evolves_to.first.evolves_to != nil
+                secondGen = evolution_chain.evolves_to.first.evolves_to
+            end
+            
+            if firstGen.first.species.name != pokemon_species.name && secondGen.first.species.name != pokemon_species.name
+                evolution = firstGen.first.species.name
+            elsif firstGen.first.species.name == pokemon_species.name
+                evolution = secondGen.first.species.name
+            end
 
-        if evolution
-            return evolution
+            if evolution
+                return evolution
+            end
+        rescue Exception => e
+            puts "Error on : " + pokemon_species.name, e
         end
         return "Nao Tem Evolucao"
     end
@@ -38,15 +50,15 @@ class V1::PokemonsController < ApplicationController
             @pokeArr << pokemon.name
             
             if id % 50 == 0 && id != 150
-                puts "API accepts only 100 req per minute. Sleeping 30s"
-                sleep(30)
+                puts "API accepts only 100 req per minute. Sleeping 60s"
+                sleep(60)
             end 
         end
         create_pokemon(@pokeArr)
     end
 
     def create_pokemon(poke)
-        id = 1
+        id = poke[0].id
         for i in 0..poke.length-1
             @img_url = "https://raw.githubusercontent.com/PokeAPI/pokeapi/master/data/v2/sprites/pokemon/" + id.to_s + ".png"
             pokemon = Pokemon.new(name: poke[i], url: @img_url, evolution: @evoArr[i])
